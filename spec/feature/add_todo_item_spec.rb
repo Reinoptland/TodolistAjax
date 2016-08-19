c91897200994b59ee85cf0c12d5dbd39cde07fd0
reinoptland <reinoptland@gmail.com>
2016-08-19 10:44:58 +0200

    Add complete task spec

--- spec/feature/add_todo_item_spec.rb
+++ spec/feature/add_todo_item_spec.rb
@@ -20,4 +20,16 @@ feature 'Manage tasks', js: true do
 
       expect( page.find(:css, 'span#todo-count').text).to eq "1"
     end
+
+    scenario 'complete a task' do
+      visit todos_path
+      fill_in 'todo_title', with: 'go to candy mountain'
+      page.execute_script("$('form').submit()")
+
+      check('todo-1')
+
+      sleep(1)
+
+      expect( page.find(:css, 'span#todo-count').text ).to eq "0"
+    end
 end
